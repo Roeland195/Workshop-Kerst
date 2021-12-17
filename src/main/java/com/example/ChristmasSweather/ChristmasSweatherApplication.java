ba8b969e8feb5d25813d7e0afda64b2b096eb5db
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2021-12-17 14:02:37 +0100

    project

---
+++ src/main/java/com/example/ChristmasSweather/ChristmasSweatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChristmasSweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasSweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasSweatherApplication.class, args);
+	}
+
+}
