891c9c1eea0fe8d74f391b377bf58a5d40d0e6ab
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-01-12 17:49:40 +0100

    product Toevoegen

--- src/main/java/com/example/ChristmasSweather/Controller/ProductController.java
+++ src/main/java/com/example/ChristmasSweather/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.ChristmasSweather.Controller;
 
+import com.example.ChristmasSweather.HTTPResponse;
 import com.example.ChristmasSweather.Models.Customer;
 import com.example.ChristmasSweather.Models.Product;
 import com.example.ChristmasSweather.Models.Shoppingcart;
@@ -18,25 +19,33 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/Product")
-    public @ResponseBody
-    String addNewProduct(@RequestParam Boolean avalable,
-                         @RequestParam String color,
-                         @RequestParam String description,
-                         @RequestParam String image,
-                         @RequestParam String name,
-                         @RequestParam double price,
-                         @RequestParam String sex,
-                         @RequestParam String size,
-                         @RequestParam int    total,
-                         @RequestParam Set<Theme> themes
-
-                         ){
-
-        Product product = new Product(name,total,description,price,color,avalable,image,sex,size,themes);
-        productRepository.save(product);
-        return "saved";
+    public HTTPResponse addScore(@RequestBody Product[] products){
+        Product product = products[0];
+        return HTTPResponse.<Product>returnSuccess(product);
     }
+
+//    @CrossOrigin(origins = "http://localhost:4200")
+//    @PostMapping("/Product")
+//    public @ResponseBody
+//    String addNewProduct(@RequestParam Boolean avalable,
+//                         @RequestParam String color,
+//                         @RequestParam String description,
+//                         @RequestParam String image,
+//                         @RequestParam String name,
+//                         @RequestParam double price,
+//                         @RequestParam String sex,
+//                         @RequestParam String size,
+//                         @RequestParam int    total,
+//                         @RequestParam Set<Theme> themes
+//
+//                         ){
+//
+//        Product product = new Product(name,total,description,price,color,avalable,image,sex,size,themes);
+//        productRepository.save(product);
+//        return "saved";
+//    }
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/Product")
