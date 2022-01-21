be35c64b60aa37abd9c400a4bbe7039015f275fd
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-01-21 23:02:37 +0100

    actuatorc

--- src/main/java/com/example/ChristmasSweather/Controller/AccountController.java
+++ src/main/java/com/example/ChristmasSweather/Controller/AccountController.java
@@ -31,45 +31,4 @@ public class AccountController {
         Address a = new Address(o.getCity(),o.getCountry(),o.getStreet(),o.getNumber(),o.getExtra());
         return accountDao.registerAccount(o.getFirstName(), o.getLastName(), o.getEmail(), o.getPassword(), a);
     }
-
-    @PostMapping("/role/save")
-    public HTTPResponse<Role> saveRole(@RequestBody Role role) {
-        return accountDao.saveRole(role);
-    }
-
-    @PostMapping("/role/addtouser")
-    public HTTPResponse<String> addRoleRoUser(@RequestBody RoleUserRequestObject form) {
-        return accountDao.addRoleToUser(form.getEmail(), form.getRoleName());
-    }
-
-    @PostMapping("/role/removefromuser")
-    public HTTPResponse<String> removeRoleFromUser(@RequestBody RoleUserRequestObject form) {
-        return accountDao.removeRoleFromUser(form.getEmail(), form.getRoleName());
-    }
-
-    @GetMapping("/account")
-    public HTTPResponse<AccountReturnObject> getAccountDetails(@RequestParam(name="id", defaultValue="") String id, @RequestParam(name="email", defaultValue = "") String email) {
-        // todo make sure top check if this is the account thats logged in!!!
-        if (id.equals(""))
-            return accountDao.getIdBelongingToEmail(email);
-        return accountDao.getAccountDetails(id);
-    }
-
-    @PutMapping("/account/mod")
-    public HTTPResponse<Account[]> changeAccount(@RequestBody Account[] accounts) {
-        if (accounts.length == 2) {
-            return accountDao.changeAccount(accounts);
-        }
-        return HTTPResponse.<AccountReturnObject>returnFailure("input length is not 2");
-    }
-
-    @GetMapping("/account/mod")
-    public HTTPResponse<List<AccountReturnObject>> getAllMods(){
-        return accountDao.getAllMods();
-    }
-
-    @PostMapping("/account/mod")
-    public HTTPResponse<AccountReturnObject> createMod(@RequestBody AccountRequestObject acc){
-        return accountDao.createMod(acc);
-    }
 }
