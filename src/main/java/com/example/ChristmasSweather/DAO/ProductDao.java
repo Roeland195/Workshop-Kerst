7db22ef7df3087b6e5de52784f44ec38ab0b67f2
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-03-28 13:30:52 +0200

    Update ProductDao.java

--- src/main/java/com/example/ChristmasSweather/DAO/ProductDao.java
+++ src/main/java/com/example/ChristmasSweather/DAO/ProductDao.java
@@ -14,6 +14,8 @@ public class ProductDao {
     @Autowired
     ProductRepository productRep;
 
+    String test = "tset";
+
     public ProductDao(){}
 
     public HTTPResponse getProduct(String name){
